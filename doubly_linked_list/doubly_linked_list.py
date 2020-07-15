23f61e5bb87ad9e3f5d1e3354a9b3e259bec0f83
Angelg10 <angelguzman97@gmail.com>
2020-07-15 13:09:36 -0400

    doubly linked list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -2,16 +2,21 @@
 Each ListNode holds a reference to its previous node
 as well as its next node in the List.
 """
+
+
 class ListNode:
     def __init__(self, value, prev=None, next=None):
         self.prev = prev
         self.value = value
         self.next = next
-            
+
+
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
 """
+
+
 class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
@@ -20,63 +25,164 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self.length
-    
+
     """
     Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly.
     """
+
     def add_to_head(self, value):
-        pass
-        
+        new_node = ListNode(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            old_head = self.head
+            self.head = new_node
+            self.head.next = old_head
+            old_head.prev = new_node
+        self.length += 1
+
     """
     Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node.
     """
+
     def remove_from_head(self):
-        pass
-            
+        if not self.head:
+            return
+        if self.head is self.tail:
+            val = self.head.value
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return val
+        old_head = self.head
+        self.head = self.head.next
+        self.head.prev = None
+        self.length -= 1
+        return old_head
+
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly.
     """
+
     def add_to_tail(self, value):
-        pass
-            
+        new_node = ListNode(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+        else:
+            old_tail = self.tail
+            self.tail = new_node
+            old_tail.next = self.tail
+            self.tail.prev = old_tail
+            self.length += 1
+
     """
     Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
+
     def remove_from_tail(self):
-        pass
-            
+        if self.length == 0:
+            return
+        elif self.length == 1:
+            val = self.head.value
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return val
+        else:
+            val = self.tail
+            self.tail = self.tail.prev
+            self.tail.next = None
+            self.length -= 1
+            return val
+
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
+
     def move_to_front(self, node):
-        pass
-        
+        if self.length <= 1 or self.head == node:
+            return
+        elif node == self.tail:
+            self.remove_from_tail()
+            self.add_to_head(node.value)
+        else:
+            self.length -= 1
+            prev = self.find_prev(node)
+            prev.next = node.next
+            node.next.prev = prev
+            self.add_to_head(node.value)
+
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
+
     def move_to_end(self, node):
-        pass
+        if self.length <= 1 or self.tail == node:
+            return
+        elif node == self.head:
+            self.remove_from_head()
+            self.add_to_tail(node.value)
+        else:
+            self.length -= 1
+            prev = self.find_prev(node)
+            prev.next = node.next
+            node.next.prev = prev
+            self.add_to_tail(node.value)
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
+
     def delete(self, node):
-        pass
+        if not self.head:
+            return None
+        if node == self.head:
+            self.remove_from_head()
+        elif node == self.tail:
+            self.remove_from_tail()
+        else:
+            prev = self.find_prev(node)
+            prev.next = node.next
+            node.next.prev = prev
+            self.length -= 1
+
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
+
     def get_max(self):
-        pass
+        if not self.head:
+            return None
+        max_value = self.head.value
+        current = self.head.next
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.next
+        return max_value
+
+    def find_prev(self, node):
+        if self.length == 0:
+            return False
+        current = self.head.next
+        while current.next is not node and current.next:
+            current = current.next
+        return current
--- stack/stack.py
+++ stack/stack.py
@@ -11,157 +11,13 @@ return elements in Last In First Out order.
    implementing a Stack?
 """
 
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-    def get_value(self):
-        return self.value
-
-    def get_next(self):
-        return self.next
-
-    def set_next(self, new_next):
-        self.next = new_next
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def add_to_tail(self, value):
-        # 1. create the Node from the value
-        new_node = Node(value)
-        # So, what do we do if tail is None?
-        # What's the rule we want to set to indicate that the linked
-        # list is empty?
-        # Would it be better to check the head?
-        # Let's check them both: an empty linked list has an empty
-        # head and an empty tail
-        if self.head is None and self.tail is None:
-            # in a one-element linked list, what should head and tail
-            # be referring to?
-            # have both head and tail referring to the single node
-            self.head = new_node
-            # set the new node to be the tail
-            self.tail = new_node
-        else:
-            # These steps assume that the tail is already referring
-            # to a Node
-            # 2. set the old tail's next to refer to the new Node
-            self.tail.set_next(new_node)
-            # 3. reassign self.tail to refer to the new Node
-            self.tail = new_node
-
-    def remove_head(self):
-        # if we have an empty linked list
-        if self.head is None and self.tail is None:
-            return
-        # what if we only have a single elem in the linked list?
-        # both head and tail are pointing at the same Node
-        if not self.head.get_next():
-            head = self.head
-            # delete the linked list's head reference
-            self.head = None
-            # also delete the linked list's tail reference
-            self.tail = None
-            return head.get_value()
-        val = self.head.get_value()
-        # set self.head to the Node after the head
-        self.head = self.head.get_next()
-        return val
-
-    def remove_tail(self):
-        # if we have an empty linked list
-        if self.head is None:
-            return
-
-        current = self.head
-
-        while current.get_next() and current.get_next() is not self.tail:
-            current = current.get_next()
-
-        # at this point, `current` is the node right before the tail
-        # set the tail to be None
-        val = self.tail.get_value()
-
-        # move self.tail to the Node right before
-        self.tail = current
-
-        # remove new tail's reference to the old tail
-        self.tail.next = None
-
-        return val
-
-    def contains(self, value):
-        if not self.head:
-            return False
-
-        # get a reference to the node we're currently at; update this as we traverse the list
-        current = self.head
-
-        # check to see if we're at a valid node
-        while current:
-
-            # return True if the current value we're looking at matches our target value
-            if current.get_value() == value:
-                return True
-
-            # update our current node to the current node's next node
-            current = current.get_next()
-
-        # if we've gotten here, then the target node isn't in our list
-        return False
-
-    def get_max(self):
-
-        if not self.head:
-            return None
-
-        # reference to the largest value we've seen so far
-        max_value = self.head.get_value()
-
-        # reference to our current node as we traverse the list
-        current = self.head.get_next()
-
-        # check to see if we're still at a valid list node
-        while current:
-
-            # check to see if the current value is greater than the max_value
-            if current.get_value() > max_value:
-                # if so, update our max_value variable
-                max_value = current.get_value()
-
-            # update the current node to the next node in the list
-            current = current.get_next()
-
-        return max_value
-
-    def print(self):
-        # get first node
-        node = self.head
-        linked_list = ''
-
-        # loop over list
-        while node:
-            # add value to the string
-            linked_list += f'{node.value} -> '
-
-            # get next node in list
-            node = node.get_next()
-
-        # print the string of values
-        print(linked_list)
-
 # Like a set of plates
 # Can't get to bottom unless you go through the top
 # Last In First Out (LIFO)
 
 # Removing items from the front of a linked list is much easier because moving the head pointer to the next node
 # is done in constant time
+from singly_linked_list import LinkedList
 
 
 class Stack:
@@ -210,3 +66,69 @@ class Stack:
 #             popped = self.storage.pop()
 #             self.size = len(self.storage)
 #             return popped
+
+# Sean's solution
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+#
+#     def __len__(self):
+#         return len(self.storage)
+#
+#     def push(self, value):
+#         # self.storage.insert(0, value) # O(n)
+#         self.storage.append(value) # O(1)
+#
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             return self.storage.pop() # O(1)
+#         return None
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+#
+#     def __len__(self):
+#         return self.size
+#
+#     def push(self, value):
+#         self.size += 1
+#         self.storage.add_to_tail(value)
+#
+#     def pop(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.remove_tail()
+#         return None
+
+
+# In arrays (lists) elements are contiguous in memory (arranged one after another)
+
+# Pros: Easier to find where everything is. We know starting from the first index and the length of the array,
+# we can jump n times to whichever value we need. eg. from 1 if len is 5, and we need 3 just jump twice to get to 3.
+
+# Cons: If memory is taken up next to our array and we want to add data we won't have contiguous chunks of memory to
+# add our data. (disk fragmentation)
+# Finding a new contiguous chunk of memory and moving all the elements over is expensive, especially when compared
+# adding an element when there already is enough space
+
+# Removing from the front of array arr.pop(0)
+# removes first index but then each element has to move back one spot. can't have empty space in array, only at the end
+# of an array
+
+
+# Array is what is call Static Data structure because the memory is static once it is allocated, the memory can't be
+# shrink or extend once it is allocated. To increase the memory size the whole array need to be copy to a new memory
+# larger this is due to the contiguous nature of array all elements has to be contiguous this why it is possible to
+# index to array and get out an element.
+
+
+# In a linked list elements (nodes) are NOT contiguous in memory
+
+# No need to pack them contiguously
+
+# remove head is O(1) constant time
+
